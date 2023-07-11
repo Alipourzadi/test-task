9733147c30776072bf40f97432526083c58e0ff9
Ap <ali.pourzadii@gmail.com>
2023-07-11 21:08:10 +0330

    last commit

--- components/Header/index.jsx
+++ components/Header/index.jsx
@@ -1,6 +1,6 @@
 export default function Header({innerText}) {
   return (
-    <h1 className="text-lg font-yekanExtraBold border-r-4 border-r-green-600 mr-6 px-2">
+    <h1 className="text-sm font-yekanExtraBold border-r-4 border-r-green-600 mr-6 px-2 sm:text-lg">
       {innerText}
     </h1>
   );
